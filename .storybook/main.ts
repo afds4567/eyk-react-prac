e44aa546f4b84688b44793ab5c6b469938c243e4
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-06-16 17:52:14 +0900

    feat: Add Button component story to Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,14 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
-const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-actions',
+    '@storybook/addon-controls'
   ],
+
   framework: {
-    name: "@storybook/react-webpack5",
-    options: {},
+    name: '@storybook/react-webpack5',
+    options: {}
   },
   docs: {
-    autodocs: "tag",
-  },
+    autodocs: true
+  }
 };
-export default config;
